4ec67531997caa35d53168a88d486bf52a5bcd6f
Darwin Risser <risser.darwin@gmail.com>
2019-01-09 08:18:55 -0600

    morning updates

--- public/index.js
+++ public/index.js
@@ -4,9 +4,13 @@ const listProducts = document.getElementById("listProducts");
 listProducts.innerHTML = products.map(item=>`<li>${item.name}</li>`).join('');
 
 //search for products
+
+const searchBox = document.getElementById('searchBox');
+const searchInput = searchBox.toLowerCase;
 const searchSubmit = document.getElementById('searchSubmit');
+const searchResults = document.getElementById('searchResults');
 
+const results = products.filter(item=>item.name.indexOf(searchInput) > -1);
 
-const searchResults = addEventListener('click', function(){
-
-})
+searchSubmit.addEventListener('click', function(){
+  searchResults.innerHTML = results.map(result=>`<p>${result.name}</p>`)})
