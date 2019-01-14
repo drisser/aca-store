0a045157dc609529fa1259d0d74564808e33e086
Darwin <risser.darwin@gmail.com>
2019-01-14 15:15:06 -0600

    monday commit

--- public/index.js
+++ public/index.js
@@ -1,5 +1,16 @@
 'use strict'
 
+let cart = [];
+
+// onload function
+
+window.onload = function(){
+    cart = JSON.parse(sessionStorage.getItem("cart"));
+    if (!cart){
+      cart = [];
+    }
+  }
+
 //list of products
 
 const listProducts = document.getElementById("listProducts");
@@ -32,19 +43,28 @@ const productDetail = document.getElementById("productDetail");
 const selectNumber = `<select><option value=1>1</option><option value=2>2</option><option value=3>3</option><option value=4>4</option><option value=5>5</option><option value=6>6</option><option value=7>7</option><option value=8>8</option><option value=9>9</option><option value=10>10</option></select>`
 
 function viewDetails(index) {
-    console.log(products[index].name)
   productDetail.innerHTML = `<h3>Details</h3><br><p>${products[index].name}</p> <p>Info: ${products[index].description}</p> <p>Type: ${products[index].category}</p> <p>Rating: ${products[index].rating}/5</p> <button>Reviews <i class="fas fa-star-half-alt"></i></button> <button onclick="addToCart(${index})">Add To Cart <i class="fas fa-cart-plus"></i></button> ${selectNumber}`;
 }
 
-// create shopping cart, add iteams to shopping cart
+// create shopping cart, add iteams to shopping cart, use sessionStorage with cart
 
 const shoppingCart = document.getElementById("shoppingCart");
-const cart = [];
-
 function addToCart(index){
-    return cart.push(products[index].name);
+    cart.push(products[index].name, products[index].price), shoppingCart.innerHTML = `<h3>Shopping Cart</h3> ${cart}`; 
+    sessionStorage.setItem("cart", JSON.stringify(cart));
 }
 
-    // shoppingCart.innerHTML = `${cart}`; 
+function viewCart(){
+    shoppingCart.innerHTML = `<h3>Shopping Cart</h3> ${cart}`
+}
+
+// use select feature by search bar
+
+const selectProduct = document.getElementById("selectProduct");
+
+function selectFood(){
+    listProducts.innerHTML = products.map(item=>`<li>${item.name}</li>`).join('');
+}
+
 
 
